513ef9cabfaabadc5a319ba9bc3a4c0a086e48d9
Jamshid <mamatovjamshid506@gmail.com>
2021-03-04 22:56:00 +0500

    database creat

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"]="sql:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///database.db"
 
 db=SQLAlchemy(app)
 
 class User_task():
     id= db.Column(db.Integer,primary_key=True)
     task=db.Column(db.String(80),unique=True)
-    state=db.Column(db.Bool)
+    state=db.Column(db.String(20))
 
 
 @app.route("/")
