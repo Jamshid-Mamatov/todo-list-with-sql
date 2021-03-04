678724428512ff95abfa9f87018f76cbef8599f1
Jamshid <mamatovjamshid506@gmail.com>
2021-03-04 22:38:02 +0500

    database creat

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"]="sql:///database.db"
+
+db=SQLAlchemy(app)
+
+class User_task():
+    id= db.Column(db.Integer,primary_key=True)
+    task=db.Column(db.String(80),unique=True)
+    state=db.Column(db.Bool)
+
+
+@app.route("/")
+def home():
+
+    return "hello"
+
+if __name__=="__main__":
+    app.run(debug=True)
